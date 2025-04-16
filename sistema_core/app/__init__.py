94e2f03af310c4164f7e40830e4f4ff273caa6ad
tebyboho <estebanbohorquez86@gmail.com>
2025-04-16 18:47:38 -0300

    Modularizacion de rutas y blueprints

--- sistema_core/app/__init__.py
+++ sistema_core/app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from .routes import routes
+from .routes.main_routes import routes
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "una_clave_segura"
-    app.register_blueprint(routes)
+    app.register_blueprint(routes)  
     return app
--- sistema_core/app/routes/main_routes.py
+++ sistema_core/app/routes/main_routes.py
@@ -2,14 +2,14 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from sqlalchemy import text
 from app.db import engine
 from werkzeug.security import check_password_hash
-routes = Blueprint('routes', __name__)
+main = Blueprint('main', __name__)
 
-@routes.route('/')
+@main.route('/')
 def login_form():
     return render_template('login.html')  # No pongas 'templates/' acá
 
 
-@routes.route('/login', methods=['POST'])
+@main.route('/login', methods=['POST'])
 def login():
     email = request.form.get('email')
     password = request.form.get('password')
@@ -18,7 +18,7 @@ def login():
     
     if not email or not password:
         flash("Se requiere email y contraseña", "danger")
-        return redirect(url_for('routes.login_form'))
+        return redirect(url_for('main.login_form'))
         
     query = text( "SELECT id, nombre, password, rol_id FROM usuarios where email = :email")
     
@@ -27,40 +27,40 @@ def login():
     
     if not result:
         flash("Usuario no encontrado", "danger")
-        return redirect(url_for("routes.login_form"))
+        return redirect(url_for("main.login_form"))
     
     user_id, nombre, password_hash, rol_id = result
     
     if not check_password_hash(password_hash, password):
         flash("Contraseña incorrecta", "danger")
-        return redirect(url_for("routes.login_form"))
+        return redirect(url_for("main.login_form"))
     
     session["user_id"] = user_id
     session["user_role"] = rol_id
     
     #empleado, encargado, admin, superusuario
     if rol_id == 1:
-        return redirect(url_for("routes.dashboard_empleado"))
+        return redirect(url_for("main.dashboard_empleado"))
     elif rol_id == 2:
-        return redirect(url_for("routes.dashboard_encargado"))
+        return redirect(url_for("main.dashboard_encargado"))
     elif rol_id == 3:
-        return redirect(url_for("routes.dashboard_administrador"))
+        return redirect(url_for("main.dashboard_administrador"))
     elif rol_id == 4:
-        return redirect(url_for("routes.dashboard_empleado"))
+        return redirect(url_for("main.dashboard_empleado"))
     
     else:
         flash("Rol no reconocido", "danger")
-        return redirect(url_for("routes.login_form"))
+        return redirect(url_for("main.login_form"))
     
 
-@routes.route('/dashboard-empleado')
+@main.route('/dashboard-empleado')
 def dashboard_empleado():
     return render_template('dashboard_empleado.html')
 
-@routes.route('/dashboard-encargado')
+@main.route('/dashboard-encargado')
 def dashboard_encargado():
     return render_template('dashboard_encargado.html')
 
-@routes.route('/dashboard-administrador')
+@main.route('/dashboard-administrador')
 def dashboard_administrador():
     return render_template('dashboard_administrador.html')
